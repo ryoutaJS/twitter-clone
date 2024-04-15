d9ec78c3745ed2ee1a72e9505c07466db9659b2e
miruwan <agou.ar1209@gmail.com>
2024-04-15 15:43:11 +0900

    feat: FooterにHomeアイコンボタンを追加
     - FooterコンポーネントにHomeアイコンボタンを表示するところまで実装

--- frontend/app/componets/elements/footer/footer.tsx
+++ frontend/app/componets/elements/footer/footer.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import HomeIcon from "@mui/icons-material/Home";
+import { Grid } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import CssBaseline from "@mui/material/CssBaseline";
+import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 
 export const Footer = () => {
@@ -9,7 +12,19 @@ export const Footer = () => {
     <>
       <CssBaseline />
       <AppBar position='fixed' color='primary' sx={{ top: "auto", bottom: 0 }}>
-        <Toolbar></Toolbar>
+        <Toolbar>
+          <Grid container>
+            <Grid item xs={3} style={{ paddingLeft: "6%" }}>
+              <IconButton
+                color='inherit'
+                aria-label='open drawer'
+                style={{ color: "white" }}
+              >
+                <HomeIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
       </AppBar>
     </>
   );
