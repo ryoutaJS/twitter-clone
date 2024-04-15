96dec15b579a568fef1fa4b9e017c476e6e7e182
miruwan <agou.ar1209@gmail.com>
2024-04-15 16:14:42 +0900

    feat: Footerにプラスアイコンボタンを追加
     - Footerコンポーネントにツイートを作成するためのプラスアイコンボタンを表示
     - プラスボタンを押下することでツイート作成画面へ遷移するところまで実装（ツイート作成画面は未実装）

--- frontend/app/componets/elements/footer/footer.tsx
+++ frontend/app/componets/elements/footer/footer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import AddIcon from "@mui/icons-material/Add";
 import HomeIcon from "@mui/icons-material/Home";
 import ModeCommentIcon from "@mui/icons-material/ModeComment";
 import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
@@ -7,8 +8,19 @@ import SearchIcon from "@mui/icons-material/Search";
 import { Grid } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import CssBaseline from "@mui/material/CssBaseline";
+import Fab from "@mui/material/Fab";
 import IconButton from "@mui/material/IconButton";
+import { styled } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
+import Link from "next/link";
+
+const StyledFab = styled(Fab)({
+  position: "absolute",
+  zIndex: 1,
+  top: -60,
+  left: "85%",
+  right: 0,
+});
 
 export const Footer = () => {
   return (
@@ -43,6 +55,11 @@ export const Footer = () => {
             </Grid>
           </Grid>
         </Toolbar>
+        <StyledFab color='primary' aria-label='add'>
+          <Link href='/tweet' style={{ textDecoration: "none" }}>
+            <AddIcon sx={{ mt: 1 }} />
+          </Link>
+        </StyledFab>
       </AppBar>
     </>
   );
