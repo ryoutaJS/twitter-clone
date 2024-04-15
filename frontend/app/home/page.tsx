6eaa8794aa8dba315295ede81a49ac6dc444f4f3
miruwan <agou.ar1209@gmail.com>
2024-04-15 14:17:46 +0900

    feat: Headerコンポーネントの作成と表示
    - 作成したHeaderコンポーネントをtweet一覧画面に表示するところまで実装

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,8 +1,23 @@
+import { Grid } from "@mui/material";
+import { Header } from "../componets/elements/header/header";
+
 const Home = async () => {
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <>
+      <Grid container>
+        <Grid item xs={12}>
+          {/* Headerコンポーネントの利用 */}
+          <Header />
+        </Grid>
+      </Grid>
+      <Grid container style={{ marginTop: "60px" }}>
+        <Grid item xs={12}>
+          <div>
+            <h1>Hello World!!!</h1>
+          </div>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
