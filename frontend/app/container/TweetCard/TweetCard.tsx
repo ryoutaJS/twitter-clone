a7a2ac781819cdb043740db8502ece2362c3c093
miruwan <agou.ar1209@gmail.com>
2024-04-16 16:38:03 +0900

    feat: ツイート一覧にアバターを追加
     - ツイート一覧にユーザーのアバターを表示機能を追加しました

--- frontend/app/container/TweetCard/TweetCard.tsx
+++ frontend/app/container/TweetCard/TweetCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { tweetData } from "@/app/type/types";
+import Avatar from "@mui/material/Avatar";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 
@@ -13,6 +14,15 @@ export const TweetCard = (props: Props) => {
   return (
     <Card>
       <CardHeader
+        avatar={
+          <Avatar
+            alt='Remy Sharp'
+            // TODO: ログイン機能を実装したらユーザーの画像を表示するように更新する
+            src={
+              "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1-768x768.png"
+            }
+          />
+        }
         title={props.tweet.tweetInfo.userName}
         subheader={props.tweet.tweetInfo.createdAt}
       />
