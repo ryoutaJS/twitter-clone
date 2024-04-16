12875bb086abcdab72a7cc8429a207d725f48f10
miruwan <agou.ar1209@gmail.com>
2024-04-16 17:12:47 +0900

    feat: ツイート一覧に3点リーダーのアイコンボタンを追加
     - 3点リーダーのアイコンボタンの表示機能を追加しました
     - 3点リーダーをクリックすることでポップアップメニューを表示する

--- frontend/app/container/TweetCard/TweetCard.tsx
+++ frontend/app/container/TweetCard/TweetCard.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { StyledMenu } from "@/app/componets/styledMenu/styledMenu";
 import { tweetData } from "@/app/type/types";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Avatar from "@mui/material/Avatar";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
+import IconButton from "@mui/material/IconButton";
+import { MenuItem } from "@mui/material";
+import { useState } from "react";
 
 type Props = {
   // 親コンポーネントから受け取ったツイートデータ
@@ -11,6 +16,15 @@ type Props = {
 };
 
 export const TweetCard = (props: Props) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Card>
       <CardHeader
@@ -22,6 +36,24 @@ export const TweetCard = (props: Props) => {
               "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1-768x768.png"
             }
           />
+        }
+        action={
+          <>
+            <IconButton aria-label='settings' onClick={handleClick}>
+              <MoreVertIcon />
+            </IconButton>
+            <StyledMenu
+              id='demo-customized-menu'
+              MenuListProps={{
+                "aria-labelledby": "demo-customized-button",
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem style={{ color: "red" }}>削除</MenuItem>
+            </StyledMenu>
+          </>
         }
         title={props.tweet.tweetInfo.userName}
         subheader={props.tweet.tweetInfo.createdAt}
