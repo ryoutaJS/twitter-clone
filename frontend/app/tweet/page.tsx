15c36c7333282db33d1daa53ed02f1eeea7e5878
miruwan <agou.ar1209@gmail.com>
2024-04-18 11:09:16 +0900

    feat: ツイート作成画面にキャンセルボタンを追加
     - キャンセルボタンを追加
     - クリックしたらツイート一覧画面へ遷移する

--- frontend/app/tweet/page.tsx
+++ frontend/app/tweet/page.tsx
@@ -1,5 +1,18 @@
+import { Button, Grid } from "@mui/material";
+import Link from "next/link";
+
 const _CreateTweet = () => {
-  return;
+  return (
+    <>
+      <Grid container mt={1}>
+        <Grid item xs={3}>
+          <Link href='/home' style={{ textDecoration: "none" }}>
+            <Button variant='text'>キャンセル</Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default _CreateTweet;
