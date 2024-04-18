a65b511167dad8218c973050887b980f2f317082
miruwan <agou.ar1209@gmail.com>
2024-04-18 14:54:09 +0900

    feat: ツイート投稿のformを作成
    - ツイート作成画面の入力値をuseFromで状態管理できるようにしました
    - formのsubmitでツイートメッセージをコンソール上に表示

--- frontend/app/tweet/page.tsx
+++ frontend/app/tweet/page.tsx
@@ -1,42 +1,51 @@
+"use client";
+
 import { Avatar, Button, Grid, TextareaAutosize } from "@mui/material";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { tweetData } from "../type/types";
 
 const _CreateTweet = () => {
+  const { register, handleSubmit } = useForm<tweetData>();
+
   return (
     <>
-      <Grid container mt={1}>
-        <Grid item xs={3}>
-          <Link href='/home' style={{ textDecoration: "none" }}>
-            <Button variant='text'>キャンセル</Button>
-          </Link>
+      <form onSubmit={handleSubmit((e) => console.log(e))}>
+        <Grid container mt={1}>
+          <Grid item xs={3}>
+            <Link href='/home' style={{ textDecoration: "none" }}>
+              <Button variant='text'>キャンセル</Button>
+            </Link>
+          </Grid>
+          <Grid item xs={9} sx={{ display: "flex", justifyContent: "end" }}>
+            <Button
+              variant='contained'
+              type='submit'
+              style={{ borderRadius: "20px" }}
+            >
+              投稿する
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={9} sx={{ display: "flex", justifyContent: "end" }}>
-          <Button
-            variant='contained'
-            type='submit'
-            style={{ borderRadius: "20px" }}
-          >
-            投稿する
-          </Button>
-        </Grid>
-      </Grid>
-      <Avatar
-        alt='Remy Sharp'
-        src={
-          "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1-768x768.png"
-        }
-      />
-      <TextareaAutosize
-        maxRows={4}
-        placeholder='いまどうしてる？'
-        style={{
-          width: "100%",
-          height: "20vh",
-          outline: "none",
-          border: "none",
-          fontSize: "15pt",
-        }}
-      />
+        <Avatar
+          alt='Remy Sharp'
+          src={
+            "https://kotonohaworks.com/free-icons/wp-content/uploads/kkrn_icon_user_1-768x768.png"
+          }
+        />
+        <TextareaAutosize
+          maxRows={4}
+          placeholder='いまどうしてる？'
+          style={{
+            width: "100%",
+            height: "20vh",
+            outline: "none",
+            border: "none",
+            fontSize: "15pt",
+          }}
+          {...register("tweetContent.message", { required: true })}
+        />
+      </form>
     </>
   );
 };
